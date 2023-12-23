6b17c6d3a3a88691bd40a4c2e7f03aaf34cdf5e5
Jackson Geer <jacksongeer@gmail.com>
2023-12-22 23:48:13 -0500

    resolved some comments

--- nvcoc_app/lib/homepages/home.dart
+++ nvcoc_app/lib/homepages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:nvcoc_app/shared/constants.dart';
 import 'package:nvcoc_app/templates/novabutton.dart';
 import 'package:nvcoc_app/templates/navbar.dart';
 import '../templates/nova_appbar.dart';
@@ -29,12 +30,13 @@ class _HomeScreenState extends State<HomeScreen> {
           child: ListView(
             children: [
               GestureDetector(
-                onTap:() => context.push('/connect'),
+                onTap: () => context.push('/connect'),
                 child: Container(
                   width: 250,
                   height: 250,
                   decoration: const BoxDecoration(
                       image: DecorationImage(
+                    colorFilter: ColorFilter.mode(novaBlue, BlendMode.color),
                     image: AssetImage('assets/churchmeeting.jpg'),
                     fit: BoxFit.cover,
                   )),
@@ -60,19 +62,19 @@ class _HomeScreenState extends State<HomeScreen> {
                             fontSize: 12.0,
                           ),
                         ),
-                        
                       ],
                     ),
                   ),
                 ),
               ),
-                GestureDetector(
-                onTap:() => context.push('/calendar'),
+              GestureDetector(
+                onTap: () => context.push('/calendar'),
                 child: Container(
                   width: 250,
                   height: 250,
                   decoration: const BoxDecoration(
                       image: DecorationImage(
+                    colorFilter: ColorFilter.mode(novaYellow, BlendMode.color),
                     image: AssetImage('assets/calendar.jpg'),
                     fit: BoxFit.cover,
                   )),
@@ -81,47 +83,48 @@ class _HomeScreenState extends State<HomeScreen> {
                     child: Text(
                       'CHECK\nOUT\nOUR\nEVENTS',
                       style: GoogleFonts.montserrat(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40
-                      ),
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40),
                     ),
                   ),
                 ),
               ),
-                GestureDetector(
-                onTap:() => context.push('/learn'),
+              GestureDetector(
+                onTap: () => context.push('/learn'),
                 child: Container(
                   width: 250,
                   height: 250,
                   decoration: const BoxDecoration(
                       image: DecorationImage(
+                    colorFilter: ColorFilter.mode(novaBlue, BlendMode.color),
                     image: AssetImage('assets/bible.jpg'),
                     fit: BoxFit.cover,
                   )),
                   child: Padding(
                     padding: const EdgeInsets.all(6.0),
                     child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [Text(
-                        'LEARN WITH US',
-                        style: GoogleFonts.montserrat(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30
-                        ),
-                      ), ]
-                    ),
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'LEARN WITH US',
+                            style: GoogleFonts.montserrat(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30),
+                          ),
+                        ]),
                   ),
                 ),
               ),
-                GestureDetector(
-                onTap:() => context.push('/belief'),
+              GestureDetector(
+                onTap: () => context.push('/belief'),
                 child: Container(
                   width: 250,
                   height: 250,
                   decoration: const BoxDecoration(
                       image: DecorationImage(
+                    colorFilter: ColorFilter.mode(novaYellow, BlendMode.color),
                     image: AssetImage('assets/beliefs.jpg'),
                     fit: BoxFit.cover,
                   )),
@@ -134,10 +137,9 @@ class _HomeScreenState extends State<HomeScreen> {
                         Text(
                           'OUR BELIEFS',
                           style: GoogleFonts.montserrat(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 40
-                          ),
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 40),
                         ),
                       ],
                     ),
