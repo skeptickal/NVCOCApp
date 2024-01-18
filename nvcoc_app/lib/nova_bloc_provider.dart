07fba5e02def3921be16ae90f3be47cb6331f324
Jackson Geer <jacksongeer@gmail.com>
2024-01-18 16:39:23 -0500

    made comment cards more  concise

--- nvcoc_app/lib/nova_bloc_provider.dart
+++ nvcoc_app/lib/nova_bloc_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:nvcoc_app/cubit/comment_cubit.dart';
+import 'package:nvcoc_app/cubits/comment_cubit.dart';
 
 class NovaBlocProvider extends StatelessWidget {
   final Widget child;
---
+++ nvcoc_app/lib/resources/comment_mixin.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:nvcoc_app/resources/constants.dart';
+
+mixin CommentMixin {
+  final TextEditingController email = TextEditingController();
+  final TextEditingController firstName = TextEditingController();
+  final TextEditingController lastName = TextEditingController();
+  final TextEditingController commenter = TextEditingController();
+  final TextEditingController address1 = TextEditingController();
+  final TextEditingController address2 = TextEditingController();
+  final TextEditingController city = TextEditingController();
+  final TextEditingController stateSelector = TextEditingController();
+  final TextEditingController zip = TextEditingController();
+  final TextEditingController phone = TextEditingController();
+  final TextEditingController email2 = TextEditingController();
+  final String country = 'US';
+  final bool value = false;
+  final EdgeInsets sideBySideText = const EdgeInsets.fromLTRB(8.0, 0, 8, 0);
+  final EdgeInsets rowPadding = const EdgeInsets.all(12.0);
+  final EdgeInsets leftPadding = const EdgeInsets.only(left: 20);
+  final EdgeInsets textBoxPadding = const EdgeInsets.fromLTRB(20.0, 12, 20, 12);
+  final EdgeInsets dropDownPadding = const EdgeInsets.fromLTRB(24, 10, 50, 10);
+  final TextStyle subTitleStyle = montserrat.copyWith(color: black, fontSize: 12, fontStyle: FontStyle.italic);
+}
