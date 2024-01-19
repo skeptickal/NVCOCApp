c6914738e11f68dbf28748bac34a610505d36d6f
Jackson Geer <jacksongeer@gmail.com>
2024-01-18 19:57:01 -0500

    added worship

--- nvcoc_app/lib/nova_bloc_provider.dart
+++ nvcoc_app/lib/nova_bloc_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nvcoc_app/cubits/comment_cubit/comment_cubit.dart';
 import 'package:nvcoc_app/cubits/housechurch_cubit/housechurch_cubit.dart';
+import 'package:nvcoc_app/cubits/worship_cubit/worship_cubit.dart';
 
 class NovaBlocProvider extends StatelessWidget {
   final Widget child;
@@ -16,6 +17,9 @@ class NovaBlocProvider extends StatelessWidget {
         ),
         BlocProvider(
           create: (context) => HousechurchCubit(),
+        ),
+        BlocProvider(
+          create: (context) => WorshipCubit(),
         ),
       ],
       child: child,
