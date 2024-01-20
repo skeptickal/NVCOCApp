c35dc374e1fb87d6fda2721f29feab762d037b30
Jackson Geer <jacksongeer@gmail.com>
2024-01-20 00:27:40 -0500

    merged version-2

--- nvcoc_app/lib/main.dart
+++ nvcoc_app/lib/main.dart
@@ -7,43 +7,10 @@ import 'package:nvcoc_app/router.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-<<<<<<< HEAD
-  runApp(MultiProvider(
-      providers: [
-        StreamProvider<List<Worship>>.value(
-          value: DatabaseService().worshipInfo,
-          catchError: (context, error) {
-            print("Error in stream: $error");
-            return [];
-          },
-          initialData: const [],
-        ),
-        StreamProvider<List<HouseChurches>>.value(
-          value: DatabaseService().houseChurches,
-          catchError: (context, error) {
-            print("Error in stream: $error");
-            return [];
-          },
-          initialData: const [],
-        ),
-        StreamProvider<List<EBulletin>>.value(
-          value: DatabaseService().eBulletinLink,
-          catchError: (context, error) {
-            print("Error in stream: $error");
-            return [];
-          },
-          initialData: const [],
-        ),
-      ],
-      child: MaterialApp.router(
-        routerConfig: router,
-      )));
-=======
   runApp(NovaBlocProvider(
     child: MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: router,
     ),
   ));
->>>>>>> version-2
 }
