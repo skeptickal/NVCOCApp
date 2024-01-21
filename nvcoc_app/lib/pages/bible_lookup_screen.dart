1999f6addbaad9c543f8289b368ded702559f3ba
Jackson Geer <jacksongeer@gmail.com>
2024-01-20 23:19:35 -0500

    converted to bible_lookup_screen.dart to readable format with BlocBuilder private classing

--- nvcoc_app/lib/pages/bible_lookup_screen.dart
+++ nvcoc_app/lib/pages/bible_lookup_screen.dart
@@ -16,87 +16,140 @@ class BibleLookupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenWrapper(
-      child: _BibleVerseLookup(),
+        child: ListView(
+      children: [
+        _TitleAndIntro(),
+        _BookInput(),
+        _ChapterVerseInput(),
+        const _BibleVerseFetchButtonAndReader(),
+      ],
+    ));
+  }
+}
+
+class _TitleAndIntro extends StatelessWidget with BibleLookupMixin {
+  _TitleAndIntro();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        seperation,
+        Center(
+          child: Text(
+            'BIBLE VERSE LOOKUP',
+            style: montserrat.copyWith(fontSize: 20, fontWeight: FontWeight.bold, color: novaBlue),
+          ),
+        ),
+        Center(
+          child: SubTitle(
+              padding: textBoxPadding,
+              textStyle: montserrat.copyWith(color: novaBlue),
+              subTitleText: 'Look up a scripture by its notation! \nVersion: World English Bible (WEB)'),
+        ),
+      ],
     );
   }
 }
 
-class _BibleVerseLookup extends StatelessWidget with BibleLookupMixin {
+class _BookInput extends StatelessWidget with BibleLookupMixin {
+  _BookInput();
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SubTitle(padding: textBoxPadding, textStyle: montserrat, subTitleText: 'Book'),
+        Padding(
+          padding: textBoxPadding,
+          child: DropdownButtonFormField<String>(
+            value: 'Select a book',
+            items: booksOfTheBible.map((book) {
+              return DropdownMenuItem<String>(
+                value: book,
+                child: Text(book),
+              );
+            }).toList(),
+            onChanged: (String? value) {
+              bookLookup.text = value ?? '';
+            },
+            decoration: textInputDecoration.copyWith(hintText: 'Select a book'),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _ChapterVerseInput extends StatelessWidget with BibleLookupMixin {
+  _ChapterVerseInput();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SubTitle(padding: textBoxPadding, textStyle: montserrat, subTitleText: 'Chapter + Verse(s)'),
+        Padding(
+          padding: textBoxPadding,
+          child: TextFormField(
+            controller: verseLookup,
+            maxLines: null,
+            keyboardType: TextInputType.multiline,
+            decoration: textInputDecoration.copyWith(hintText: 'e.g. 3:16-17'),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _SearchButton extends StatelessWidget with BibleLookupMixin {
+  final BibleState state;
+  _SearchButton({required this.state});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () async {
+        FocusScope.of(context).unfocus();
+        context.read<BibleCubit>().searchVerse('${bookLookup.text}+${verseLookup.text}');
+      },
+      child: const Text('Fetch Bible Verse'),
+    );
+  }
+}
+
+class _ScriptureOutput extends StatelessWidget {
+  final BibleState state;
+  const _ScriptureOutput({required this.state});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+        state.verse ?? "N/A",
+        style: montserrat,
+      ),
+    );
+  }
+}
+
+class _BibleVerseFetchButtonAndReader extends StatelessWidget {
+  const _BibleVerseFetchButtonAndReader();
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<BibleCubit, BibleState>(
-      builder: (context, state) {
-        TextButton searcher = TextButton(
-          onPressed: () async {
-            FocusScope.of(context).unfocus();
-            context.read<BibleCubit>().searchVerse('${bookLookup.text}+${verseLookup.text}');
-          },
-          child: const Text('Fetch Bible Verse'),
-        );
-        Text bibleVerseReader = Text(
-          state.verse ?? "N/A",
-          style: montserrat,
-        );
-
-        return ListView(
+      builder: (BuildContext context, BibleState state) {
+        return Column(
           children: [
-            seperation,
-            Center(
-              child: Text(
-                'BIBLE VERSE LOOKUP',
-                style: montserrat.copyWith(fontSize: 20, fontWeight: FontWeight.bold, color: novaBlue),
-              ),
-            ),
-            Center(
-              child: SubTitle(
-                  padding: textBoxPadding,
-                  textStyle: montserrat.copyWith(color: novaBlue),
-                  subTitleText: 'Look up a scripture by its notation! \nVersion: World English Bible (WEB)'),
-            ),
-            SubTitle(padding: textBoxPadding, textStyle: montserrat, subTitleText: 'Book'),
-            Padding(
-              padding: textBoxPadding,
-              child: DropdownButtonFormField<String>(
-                value: 'Select a book',
-                items: booksOfTheBible.map((book) {
-                  return DropdownMenuItem<String>(
-                    value: book,
-                    child: Text(book),
-                  );
-                }).toList(),
-                onChanged: (String? value) {
-                  bookLookup.text = value ?? '';
-                },
-                decoration: textInputDecoration.copyWith(hintText: 'Select a book'),
-              ),
-            ),
-            SubTitle(padding: textBoxPadding, textStyle: montserrat, subTitleText: 'Chapter + Verse(s)'),
-            Padding(
-              padding: textBoxPadding,
-              child: TextFormField(
-                controller: verseLookup,
-                maxLines: null,
-                keyboardType: TextInputType.multiline,
-                decoration: textInputDecoration.copyWith(hintText: 'e.g. 3:16-17'),
-              ),
-            ),
-            searcher,
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: bibleVerseReader,
-            ),
+            _SearchButton(state: state),
+            _ScriptureOutput(state: state),
           ],
         );
       },
     );
-  }
-}
-
-class _TitleAndIntro extends StatelessWidget {
-  const _TitleAndIntro();
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
