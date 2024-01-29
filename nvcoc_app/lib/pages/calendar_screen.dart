fb984a75c560884b170a16896ad88947f39fe948
Jackson Geer <jacksongeer@gmail.com>
2024-01-28 22:55:19 -0500

    added calendar_screen_test and began comment_screen_test

--- nvcoc_app/lib/pages/calendar_screen.dart
+++ nvcoc_app/lib/pages/calendar_screen.dart
@@ -19,12 +19,14 @@ class CalendarScreen extends StatelessWidget {
             seperation,
             Center(
               child: Text(
+                key: const Key('calendar_title'),
                 'CALENDAR OPTIONS',
                 style: montserrat.copyWith(fontSize: 20, fontWeight: FontWeight.bold, color: novaBlue),
               ),
             ),
             Center(
               child: SubTitle(
+                key: const Key('calendar_subtitle'),
                 padding: const EdgeInsets.all(14),
                 textStyle: montserrat.copyWith(color: novaBlue, fontStyle: FontStyle.italic),
                 subTitleText: 'Click any Calendar option below to view our online Calendars',
@@ -33,14 +35,17 @@ class CalendarScreen extends StatelessWidget {
             horizontalLine,
             seperation,
             const CustomTextLaunchButton(
+              key: Key('calendar_link'),
               url: 'https://www.nvcoc.church/churchcalendar',
               title: 'Church Calendar',
             ),
             const CustomTextLaunchButton(
+              key: Key('featured_events_link'),
               url: 'https://www.nvcoc.church/featured-events',
               title: 'Featured Events',
             ),
             const CustomTextLaunchButton(
+              key: Key('google_sheets_link'),
               url: 'https://docs.google.com/spreadsheets/d/1sdPuWX3xtC9JKrvL_UVVMPz56Oh0iDqThtkXHTL6xmo/edit#gid=0',
               title: 'Google Sheet - Year Calendar',
             ),
