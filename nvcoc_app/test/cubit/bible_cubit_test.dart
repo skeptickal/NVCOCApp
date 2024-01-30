4d8da882269a813682d04aa6f828ebb23052d4ae
Jackson Geer <jacksongeer@gmail.com>
2024-01-29 20:25:13 -0500

    added cubit tests

---
+++ nvcoc_app/test/cubit/bible_cubit_test.dart
@@ -0,0 +1,32 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:nvcoc_app/cubits/bible_cubit/bible_cubit.dart';
+import '../mocks.dart';
+
+main() {
+  group(
+    'Bible Cubit Tests',
+    () {
+      late BibleCubit bibleCubit;
+      late MockBibleQuery mockBibleQuery;
+
+      setUp(() {
+        mockBibleQuery = MockBibleQuery();
+        bibleCubit = BibleCubit(bibleQuery: mockBibleQuery);
+      });
+
+      blocTest('Search Term returns valid verse',
+          setUp: () {
+            when(() => mockBibleQuery.getPassage(any())).thenAnswer(
+              (_) => Future.value('John 3:16'),
+            );
+          },
+          build: () => bibleCubit,
+          act: (cubit) async {
+            await cubit.searchVerse('');
+          },
+          expect: () => [const BibleState(verse: 'John 3:16')]);
+    },
+  );
+}
