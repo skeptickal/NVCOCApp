4ac92ee103f8cac7c78d405998f27e5c2774462b
Jackson Geer <jacksongeer@gmail.com>
2024-01-29 21:03:17 -0500

    removed unneccessary copyWith function in Bible State

--- nvcoc_app/lib/cubits/bible_cubit/bible_state.dart
+++ nvcoc_app/lib/cubits/bible_cubit/bible_state.dart
@@ -6,10 +6,6 @@ class BibleState extends Equatable {
 
   const BibleState({this.verse});
 
-  BibleState copyWith({verse}) {
-    return BibleState(verse: verse ?? this.verse);
-  }
-
   @override
   List<Object?> get props => [verse];
 }
