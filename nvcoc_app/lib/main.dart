1269827bcd0b3ef673ff0ada5e0c4fb598d513c5
Jackson Geer <jacksongeer@gmail.com>
2024-02-05 14:07:01 -0500

    added push notifications

--- nvcoc_app/lib/main.dart
+++ nvcoc_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:nvcoc_app/client/firebase_client.dart';
 import 'package:nvcoc_app/firebase_options.dart';
 import 'package:nvcoc_app/nova_bloc_provider.dart';
 import 'package:nvcoc_app/router.dart';
@@ -7,6 +8,7 @@ import 'package:nvcoc_app/router.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseClient().initNotifications();
   runApp(
     NovaBlocProvider(
       child: MaterialApp.router(
