a20a726f55355aa1d7ae33ca9e0742f94db18ca6
Jackson Geer <jacksongeer@gmail.com>
2024-02-12 20:38:37 -0500

    added placeholder for SCUAD screen

---
+++ nvcoc_app/lib/pages/scuad_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScuadScreen extends StatelessWidget {
+  const ScuadScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
